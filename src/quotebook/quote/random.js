47c01d7d60b8e620307224736af8e2b9555dc61c
ayushpaultirkey <a>
2023-07-03 23:25:16 +0530

    random.js updated

--- src/quotebook/quote/random.js
+++ src/quotebook/quote/random.js
@@ -7,27 +7,27 @@ function Random(request, response) {
     
     const connection = mysql.createConnection(process.env.DATABASE_URL);
     
-        connection.connect((error) => {
-    
-            if(error) {
-                response.send({ ... _response, message: "Unable to connect to database" });
-            }
-            else {
-    
-                connection.query(`SELECT content AS q_content, author AS q_author FROM quotebook ORDER BY RAND() LIMIT 1;`, (error, result) => {
+    connection.connect((error) => {
 
-                    if(error) {
-                        response.send({ ... _response, message: "Unable to get quote" });
-                    }
-                    else {
-                        response.send({ ... _response, success: true, data: result });
-                    };
+        if(error) {
+            response.send({ ... _response, message: "Unable to connect to database" });
+        }
+        else {
 
-                });
-    
-            };
-    
-        });
+            connection.query(`SELECT content AS q_content, author AS q_author FROM quotebook ORDER BY RAND() LIMIT 1;`, (error, result) => {
+
+                if(error) {
+                    response.send({ ... _response, message: "Unable to get quote" });
+                }
+                else {
+                    response.send({ ... _response, success: true, data: result });
+                };
+
+            });
+
+        };
+
+    });
 
 };
 
