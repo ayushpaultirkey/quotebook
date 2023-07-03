de6a185043ba3320727e5f6a9373a0dee9c72f88
ayushpaultirkey <a>
2023-07-03 23:37:02 +0530

    menu.js github link updated

--- public/script/component/menu.js
+++ public/script/component/menu.js
@@ -15,7 +15,7 @@ class Menu extends H12.Component {
             <div class="absolute w-full h-full p-6 z-[95] bg-gray-100 top-0" style="left: {q-left}; scale: {q-scale}; transition: 0.3s;">
                 <div class="flex flex-col mt-8">
                     <label class="text-2xl font-bold">quotebook.<label class="text-sm">v1</label></label>
-                    <a href="https://github.com/ayushpaultirkey" class="text-blue-400 underline">github</a>
+                    <a href="https://github.com/ayushpaultirkey/quotebook" class="text-blue-400 underline">github</a>
                 </div>
             </div>
         </>;
